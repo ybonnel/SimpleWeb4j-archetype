12bf5c2f1489b8e53ac7cb049f180cac8027f0ec
ybonnel <ybonnel@gmail.com>
2013-03-19 08:22:07 +0100

    Some fix after first test

--- src/main/resources/archetype-resources/src/main/resources/public/js/__artifactId__.js
+++ src/main/resources/archetype-resources/src/main/resources/public/js/__artifactId__.js
@@ -7,7 +7,7 @@ function MainCtrl(${symbol_dollar}scope, ${symbol_dollar}http, ${symbol_dollar}l
 }
 MainCtrl.$inject = [ '${symbol_dollar}scope', '${symbol_dollar}http', '${symbol_dollar}log' ];
 
-angular.module('${artifactid}', []).config(['${symbol_dollar}routeProvider', function($routeProvider) {
+angular.module('${artifactId}', []).config(['${symbol_dollar}routeProvider', function($routeProvider) {
     ${symbol_dollar}routeProvider.when('/main', {templateUrl:'partial/main.html', controller:MainCtrl});
     ${symbol_dollar}routeProvider.otherwise({redirectTo: '/main'});
 }]);
