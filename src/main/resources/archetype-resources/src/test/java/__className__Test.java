3759f1a67e8e5e2c0fc443fdfdcbd7da1e30e099
ybonnel <ybonnel@gmail.com>
2013-04-30 16:12:11 +0200

    Add Fluentlenium test with SimpleWeb4j-test

--- src/main/resources/archetype-resources/src/test/java/__className__Test.java
+++ src/main/resources/archetype-resources/src/test/java/__className__Test.java
@@ -10,6 +10,7 @@ import com.google.gson.reflect.TypeToken;
 #if( ${withHibernate} == true)
 import fr.ybonnel.simpleweb4j.model.SimpleEntityManager;
 #end
+import fr.ybonnel.simpleweb4j.test.SimpleWeb4jTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,14 +22,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-public class ${className}Test {
-
-    private int port;
+public class ${className}Test extends SimpleWeb4jTest {
 
     @Before
     public void setup() {
-        port = Integer.getInteger("test.http.port", 9999);
-        ${className}.startServer(port, false);
+        ${className}.startServer(getPort(), false);
 #if( ${withHibernate} == true)
         SimpleEntityManager.openSession().beginTransaction();
         for (Hello hello : Hello.simpleEntityManager.getAll()) {
@@ -48,7 +46,7 @@ public class ${className}Test {
 
     @Test
     public void should_return_no_hello() {
-        List<Hello> hellos = new Gson().fromJson(HttpRequest.get("http://localhost:" + port + "/hello").body(), new TypeToken<List<Class>>(){}.getType());
+        List<Hello> hellos = new Gson().fromJson(HttpRequest.get(defaultUrl() + "/hello").body(), new TypeToken<List<Class>>(){}.getType());
         assertTrue(hellos.isEmpty());
     }
 
@@ -56,9 +54,9 @@ public class ${className}Test {
         Hello hello = new Hello();
         hello.setName("name");
         Gson gson = new Gson();
-        assertEquals(201, HttpRequest.post("http://localhost:" + port + "/hello").send(gson.toJson(hello)).code());
+        assertEquals(201, HttpRequest.post(defaultUrl() + "/hello").send(gson.toJson(hello)).code());
 
-        List<Hello> hellos = gson.fromJson(HttpRequest.get("http://localhost:" + port + "/hello").body(), new TypeToken<List<Hello>>(){}.getType());
+        List<Hello> hellos = gson.fromJson(HttpRequest.get(defaultUrl() + "/hello").body(), new TypeToken<List<Hello>>(){}.getType());
         assertEquals(1, hellos.size());
         assertEquals("name", hellos.get(0).getName());
         return hellos.get(0).getId();
@@ -68,7 +66,7 @@ public class ${className}Test {
     public void should_create_and_get_by_id() {
         long id = insertHello();
 
-        Hello newHello = new Gson().fromJson(HttpRequest.get("http://localhost:" + port + "/hello/" + id).body(), Hello.class);
+        Hello newHello = new Gson().fromJson(HttpRequest.get(defaultUrl() + "/hello/" + id).body(), Hello.class);
         assertNotNull(newHello);
         assertEquals(id, newHello.getId().longValue());
         assertEquals("name", newHello.getName());
@@ -82,9 +80,9 @@ public class ${className}Test {
         Hello newHello = new Hello();
         newHello.setName("newName");
 
-        assertEquals(204, HttpRequest.put("http://localhost:" + port + "/hello/" + id).send(gson.toJson(newHello)).code());
+        assertEquals(204, HttpRequest.put(defaultUrl() + "/hello/" + id).send(gson.toJson(newHello)).code());
 
-        List<Hello> hellos = gson.fromJson(HttpRequest.get("http://localhost:" + port + "/hello").body(), new TypeToken<List<Hello>>(){}.getType());
+        List<Hello> hellos = gson.fromJson(HttpRequest.get(defaultUrl() + "/hello").body(), new TypeToken<List<Hello>>(){}.getType());
         assertEquals(1, hellos.size());
         assertEquals("newName", hellos.get(0).getName());
     }
@@ -94,8 +92,8 @@ public class ${className}Test {
         long id = insertHello();
         Gson gson = new Gson();
 
-        assertEquals(204, HttpRequest.delete("http://localhost:" + port + "/hello/" + id).code());
-        List<Hello> hellos = gson.fromJson(HttpRequest.get("http://localhost:" + port + "/hello").body(), new TypeToken<List<Hello>>(){}.getType());
+        assertEquals(204, HttpRequest.delete(defaultUrl() + "/hello/" + id).code());
+        List<Hello> hellos = gson.fromJson(HttpRequest.get(defaultUrl() + "/hello").body(), new TypeToken<List<Hello>>(){}.getType());
         assertTrue(hellos.isEmpty());
     }
 
---
+++ src/main/resources/archetype-resources/src/test/java/__className__WebTest.java
@@ -0,0 +1,84 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+
+package ${package};
+
+#if( ${withHibernate} == true)
+import fr.ybonnel.simpleweb4j.model.SimpleEntityManager;
+#end
+import fr.ybonnel.simpleweb4j.test.SimpleWeb4jTest;
+import org.fluentlenium.core.domain.FluentList;
+import org.fluentlenium.core.domain.FluentWebElement;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static fr.ybonnel.simpleweb4j.SimpleWeb4j.stop;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class ${className}WebTest extends SimpleWeb4jTest {
+
+    @Before
+    public void setup() {
+        ${className}.startServer(getPort(), false);
+#if( ${withHibernate} == true)
+        SimpleEntityManager.openSession().beginTransaction();
+        for (Hello hello : Hello.simpleEntityManager.getAll()) {
+            Hello.simpleEntityManager.delete(hello.getId());
+        }
+        SimpleEntityManager.getCurrentSession().getTransaction().commit();
+        SimpleEntityManager.closeSession();
+#else
+        HelloResource.entities.clear();
+#end
+        goTo("/");
+    }
+
+    @After
+    public void tearDown() {
+            stop();
+    }
+
+    @Test
+    public void should_not_have_hello() {
+        assertThat(find("tbody tr")).isEmpty();
+    }
+
+    private void insertHello(String nameOfHello) {
+        click("#addHello");
+        fill("#name").with(nameOfHello);
+        click("#submit");
+    }
+
+    @Test
+    public void can_insert_hello() {
+        insertHello("name");
+        FluentList<FluentWebElement> trInTbody = find("tbody tr");
+        assertThat(trInTbody).hasSize(1);
+        FluentWebElement oneHello = trInTbody.get(0);
+        assertThat(oneHello.findFirst("td").getText()).contains("name");
+    }
+
+    @Test
+    public void can_update_hello() {
+        insertHello("name");
+        click("a.icon-edit");
+        clear("#name");
+        fill("#name").with("newName");
+        click("#submit");
+        FluentList<FluentWebElement> trInTbody = find("tbody tr");
+        assertThat(trInTbody).hasSize(1);
+        FluentWebElement oneHello = trInTbody.get(0);
+        assertThat(oneHello.findFirst("td").getText()).contains("newName");
+    }
+
+    @Test
+    public void can_delete_hello() {
+        insertHello("name");
+        click("a.icon-remove");
+        click("#remove");
+        assertThat(find("tbody tr")).isEmpty();
+    }
+
+}
